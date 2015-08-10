6f24a8c01718daf167861249f085362c0a39cafd
Eric Johnson <ecj@techie.com>
2015-08-10 17:16:04 -0500

    Pass arguments to methods

--- includes/classes/hook.php
+++ includes/classes/hook.php
@@ -5,21 +5,24 @@ class Hook {
   private $actions;
   private $methods;
   private $objects;
+  private $arguments;
 
   public function __construct() {
 
     $this->actions = array();
     $this->methods = array();
     $this->objects = array();
+    $this->arguments = array();
   }
 
-  public function addAction($action, $object, $method) {
+  public function addAction($action, $object, $method, $argument = null) {
 
     if (!in_array($action, $this->actions)) {
 
       array_push($this->actions, $action);
       array_push($this->methods, $method);
       array_push($this->objects, $object);
+      array_push($this->arguments, $argument);
     }
   }
 
@@ -31,10 +34,11 @@ class Hook {
 
       $method = $this->methods[$array_position];
       $object = $this->objects[$array_position];
+      $argument = $this->arguments[$array_position];
 
       if (method_exists($object, $method)) {
 
-        return call_user_func(array($object, $method));
+        return call_user_func(array($object, $method), $argument);
       }
     }
   }
