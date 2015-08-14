c34b3ed36cf9f859582c7f35482c432335bfdb54
Eric Johnson <ecj@techie.com>
2015-08-14 10:50:50 -0500

    Be able to activate extensions

---
+++ admin/activate_extension.php
@@ -0,0 +1,179 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION["username"])) {
+
+  header("Location: ./login.php");
+}
+
+require "../includes/configuration.php";
+
+require "../includes/classes/utility.php";
+require "../includes/classes/database.php";
+
+require "../includes/classes/hook.php";
+require "../includes/classes/output.php";
+
+global $Hook;
+
+$Hook = new Hook;
+
+$Output = new Output;
+$Utility = new Utility;
+$Database = new Database;
+
+$Database->connect();
+
+$Output->setDatabaseHandle($Database->getHandle());
+
+$Output->startBuffer();
+
+$statement = "
+
+  SELECT body
+  FROM " . DB_PREF . "tags
+  WHERE title = 'admin_theme_name'
+  ORDER BY id DESC
+";
+
+$query = $Database->getHandle()->query($statement);
+
+if (!$query || $query->rowCount() == 0) {
+
+  // Query failed or returned zero rows.
+  $Utility->displayError("failed to get admin theme name");
+}
+
+// Get the admin theme name.
+$theme_name = $query->fetch(PDO::FETCH_OBJ)->body;
+
+if (!file_exists("content/themes/{$theme_name}/template.html")) {
+
+  // Theme template does not exist.
+  $Utility->displayError("theme template file does not exist");
+}
+
+// Display the theme contents.
+echo file_get_contents("content/themes/{$theme_name}/template.html");
+
+$page_body = "";
+
+$page_title = "Activate Extension";
+
+if (isset($_GET["title"])) {
+
+  $statement = "
+
+    SELECT title
+    FROM " . DB_PREF . "extensions
+    WHERE title = ?
+  ";
+
+  $query = $Database->getHandle()->prepare($statement);
+
+  // Prevent SQL injections.
+  $query->bindParam(1, $_GET["title"]);
+
+  $query->execute();
+
+  if (!$query) {
+
+    // Query failed.
+    $page_body = "A database error occurred...";
+    $page_body .= "<a href=\"extensions.php\" class=\"button_return\">Return</a>";
+  }
+  else if ($query->rowCount() == 0) {
+
+    // Extension doesn't exist in database.
+    $statement = "
+
+      INSERT INTO " . DB_PREF . "extensions (
+
+        title,
+
+        activate
+      )
+      VALUES (
+
+        ?,
+
+        1
+      )
+    ";
+
+    $query = $Database->getHandle()->prepare($statement);
+
+    // Prevent SQL injections.
+    $query->bindParam(1, $_GET["title"]);
+
+    $query->execute();
+
+    if (!$query) {
+
+      $page_body = "Error: failed to activate extension!";
+      $page_body .= "<a href=\"extensions.php\" class=\"button_return\">Return</a>";
+    }
+    else {
+
+      $page_body = "The extension has been activated.";
+      $page_body .= "<a href=\"extensions.php\" class=\"button_return\">Return</a>";
+    }
+  }
+  else {
+
+    // Fetch result as object.
+    $result = $query->fetch(PDO::FETCH_OBJ);
+
+    // Get extension title.
+    $title = $result->title;
+
+    // Activate extension.
+    $statement = "
+
+      UPDATE " . DB_PREF . "extensions
+      SET activate = '1'
+      WHERE title = '{$title}'
+    ";
+
+    $query = $Database->getHandle()->query($statement);
+
+    if (!$query) {
+
+      $page_body = "Error: failed to activate extension!";
+      $page_body .= "<a href=\"extensions.php\" class=\"button_return\">Return</a>";
+    }
+    else {
+
+      $page_body = "The extension has been activated.";
+      $page_body .= "<a href=\"extensions.php\" class=\"button_return\">Return</a>";
+    }
+  }
+}
+else {
+
+  $page_body = "Error: no extension title specified!";
+  $page_body .= "<a href=\"extensions.php\" class=\"button_return\">Return</a>";
+}
+
+$Output->addTagReplacement(
+
+  "page_body",
+
+  $page_body
+);
+
+$Output->addTagReplacement(
+
+  "page_title",
+
+  $page_title
+);
+
+$Output->replaceTags();
+
+$Output->flushBuffer();
+
+$Database->disconnect();
+
+?>
