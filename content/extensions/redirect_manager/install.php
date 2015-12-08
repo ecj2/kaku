aa77587d7a2bb32ad721e27408500b73d34707eb
Eric Johnson <ecj@techie.com>
2015-12-08 14:57:14 -0600

    Add post redirect manager extension
    This extension uses simple syntax to perform 301 redirects on posts. By
    default, the extension is not active.

---
+++ content/extensions/redirect_manager/install.php
@@ -0,0 +1,44 @@
+<?php
+
+if (!$Database->checkTableExistence("extension_redirect_manager")) {
+
+  // Create the extension_redirect_manager table.
+  if (!$Database->performQuery(
+
+    "CREATE TABLE " . DB_PREF . "extension_redirect_manager (
+
+      redirect_rules TEXT NOT NULL
+    )"
+  )) {
+
+    array_push(
+
+      $errors,
+
+      "failed to create " . DB_PREF . "extension_redirect_manager table"
+    );
+  }
+
+  // Set a default value for the redirect rules
+  if (!$Database->performQuery(
+
+    "INSERT INTO " . DB_PREF . "extension_redirect_manager (
+
+      redirect_rules
+    )
+    VALUES (
+
+      'old-post-url = new-post-url;'
+    )"
+  )) {
+
+    array_push(
+
+      $errors,
+
+      "failed to insert into " . DB_PREF . "extension_redirect_manager"
+    );
+  }
+}
+
+?>
---
+++ content/extensions/redirect_manager/redirect_manager.php
@@ -0,0 +1,99 @@
+<?php
+
+// Prevent direct access to this file.
+if (!defined("KAKU_EXTENSION")) exit();
+
+$name = "Redirect Manager";
+
+class RedirectManager extends Utility {
+
+  private $DatabaseHandle;
+
+  public function __construct() {
+
+    //
+  }
+
+  public function getTags() {
+
+    return [
+
+      "body_content"
+    ];
+  }
+
+  public function getReplacements() {
+
+    return [
+
+      $this->manageRedirects()
+    ];
+  }
+
+  public function setDatabaseHandle($Handle) {
+
+    $this->DatabaseHandle = $Handle;
+  }
+
+  private function manageRedirects() {
+
+    if (isset($_GET["post_url"])) {
+
+      // Select the redirect rules from the database.
+      $statement = "
+
+        SELECT redirect_rules
+        FROM " . DB_PREF . "extension_redirect_manager
+        WHERE 1 = 1
+        LIMIT 1
+      ";
+
+      $query = $this->DatabaseHandle->query($statement);
+
+      if (!$query || $query->rowCount() == 0) {
+
+        // Query failed or returned an empty set.
+      }
+      else {
+
+        // Fetch the result as an object.
+        $result = $query->fetch(PDO::FETCH_OBJ);
+
+        preg_match_all(
+
+          "/(.+?) = (.+?)\;/",
+
+          $result->redirect_rules,
+
+          $redirect
+        );
+
+        for ($i = 0; $i < count($redirect[0]); ++$i) {
+
+          $this_url = Utility::getRootAddress() . "/post/";
+          $this_url .= $_GET["post_url"];
+
+          if (strstr($this_url, $redirect[1][$i])) {
+
+            $new_url = str_replace(
+
+              $_GET["post_url"],
+
+              $redirect[2][$i],
+
+              $this_url
+            );
+
+            header("HTTP/1.1 301 Moved Permanently");
+
+            header("Location: {$new_url}");
+
+            exit();
+          }
+        }
+      }
+    }
+  }
+}
+
+?>
