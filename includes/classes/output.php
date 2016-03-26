3c28b8763bfb41ae88291bacf978d126e289c57f
Eric Johnson <ecj@techie.com>
2016-03-26 15:00:01 -0500

    Calculate array differences outside of reset
    PHP was throwing an error saying "only variables should be passed by
    reference" where reset() was being called. By calling array_diff
    outside of reset(), PHP won't throw an error.

--- includes/classes/output.php
+++ includes/classes/output.php
@@ -143,8 +143,10 @@ class Output extends Utility {
         // Require extension source file.
         require_once $extension_full_path;
 
+        $classes = array_diff(get_declared_classes(), $classes);
+
         // Get name of newly required class.
-        $class_name = reset(array_diff(get_declared_classes(), $classes));
+        $class_name = reset($classes);
 
         if (!in_array($class_name, $this->class_name)) {
 
