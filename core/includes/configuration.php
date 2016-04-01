1a55fc2aa660c7890b64c6beb331df12091dc546
Eric Johnson <ecj@techie.com>
2016-04-01 14:27:40 -0500

    Add new definitions
    Add definitions for quick directory access and for accessing includes.

--- core/includes/configuration.php
+++ core/includes/configuration.php
@@ -1,6 +1,13 @@
 <?php
 
-// Database configurations.
+// Allow Kaku to access include files.
+define("KAKU_ACCESS", true);
+
+// Directory definitions.
+define("KAKU_ROOT", dirname(__DIR__, 2));
+define("KAKU_CORE", KAKU_ROOT . "/core");
+
+// Database definitions.
 define("DB_HOST", "localhost");
 define("DB_NAME", "");
 define("DB_USER", "");
