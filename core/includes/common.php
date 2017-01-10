072dc60d75d6b03eb3fcfb0e59768a940d763187
Eric Johnson <ecj@techie.com>
2017-01-09 19:30:11 -0600

    Remove path component code
    The path component code is no longer necessary, as Kaku will soon sport
    a more dynamic rewrite path system.

--- core/includes/common.php
+++ core/includes/common.php
@@ -2,52 +2,36 @@
 
 require "configuration.php";
 
-foreach (glob(dirname(dirname(__DIR__)) . "/core/classes/*.php") as $class) {
+$classes = glob(KAKU_ROOT . "/core/classes/*.php");
+
+// Sort classes in reverse alphabetical order.
+rsort($classes);
+
+foreach ($classes as $class) {
 
   $declared_classes = get_declared_classes();
 
-  // Load the class file.
   require $class;
 
   $class_difference = array_diff(get_declared_classes(), $declared_classes);
 
-  // Get the name of the freshly-required class.
   $class_name = reset($class_difference);
 
-  // Instantiate the class file.
+  // Instantiate each class as an object, and name the object using the class name.
   $$class_name = new $class_name;
 }
 
-if (isset($_GET["path"])) {
+if (file_exists(KAKU_ROOT . "/core/includes/install.php")) {
 
-  // Break the path components up into an array.
-  $path = explode("/", $_GET["path"]);
-
-  for ($i = 0; $i < count($path); ++$i) {
-
-    if ($i % 2) {
-
-      continue;
-    }
-
-    if (array_key_exists($i + 1, $path)) {
-
-      // Add onto the array using the first part as the key and the second as the value.
-      $path[$path[$i]] = $path[$i + 1];
-    }
-  }
-
-  // Add the assembled path components to the superglobal.
-  $_GET += $path;
+  // Install Kaku.
+  require KAKU_ROOT . "/core/includes/install.php";
 }
+
+// Clear these tags if they go unused.
+$Hook->addAction("body_content", "");
+$Hook->addAction("head_content", "");
 
 // Replace the protocol with the appropriate value.
 $Hook->addAction("protocol", $Utility->getProtocol());
-
-if (file_exists("core/includes/install.php")) {
-
-  // Install Kaku.
-  require "core/includes/install.php";
-}
 
 ?>
